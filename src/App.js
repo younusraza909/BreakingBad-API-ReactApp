f65fd8e96649faea0cd1505bfc3f62e89fba1908
younusraza909 <younusraza909@gmail.com>
2020-07-03 09:51:41 -0700

    Search And Spinner Component

--- src/App.js
+++ src/App.js
@@ -4,26 +4,29 @@ import axios from "axios";
 
 import Header from "./components/ui/Header";
 import CharacterGrid from "./components/characters/CharacterGrid";
+import Search from "./components/ui/Search";
 
 const App = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState("");
 
   //We cant use async directly on useEffect we have to use another function for it
   useEffect(() => {
     const fetchItems = async () => {
       const result = await axios(
-        `https://www.breakingbadapi.com/api/characters`
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
       );
       setItems(result.data);
       setIsLoading(false);
     };
     fetchItems();
-  }, []);
+  }, [query]);
 
   return (
     <div className="container">
       <Header />
+      <Search getQuery={(q) => setQuery(q)} />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
